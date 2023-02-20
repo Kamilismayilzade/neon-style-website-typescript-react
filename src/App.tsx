3b4ba3f23cd89af09de8162a7c4478608e5102aa
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-20 16:59:38 +0400

    Navbar imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from '@/components/Navbar/Navbar'
 
 function App() {
 
   return (
 
-    <div>app</div>
+    <div className='app bg-zinc-900 text-slate-50'>
+      
+      <Navbar />
+
+    </div>
 
   )
 }
