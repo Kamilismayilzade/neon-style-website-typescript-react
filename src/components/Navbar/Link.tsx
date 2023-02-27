0150276e9982d589f682d932b3bf3f7bc6ce4b0e
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-27 16:50:11 +0400

    pages

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -4,14 +4,20 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 type LinkProps = {
 
   page: string,
+  selectPage: string,
+  setSelectPage: (value: string)=> void,
 
 }
 
-const Link = ({ page }: LinkProps) => {
+const Link = ({ page, selectPage, setSelectPage }: LinkProps) => {
+
+
+    const pageLowerCase = page.toLowerCase().replace(/ /g, '');
+
 
   return (
 
-      <AnchorLink>
+      <AnchorLink className={`${selectPage === pageLowerCase  ? 'text-fuchsia-500' : ''}  transition duration-500 hover: text-fuchsia-300 `} href={`#${pageLowerCase}`} onClick={ ()=> setSelectPage(pageLowerCase) } >
           {page}
       </AnchorLink>
     
