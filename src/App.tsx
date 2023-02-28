4e22b7ae2d849014130fd39d97090f71c3e5c06b
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-28 12:38:02 +0400

    identifying top of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from '@/components/Navbar/Navbar'
 import { SelectPage } from '@/shared/types';
+import Home from '@/components/Home/Home';
 
 
 
 function App() {
 
   const [selectPage, setSelectPage] = useState<SelectPage>(SelectPage.Home);
+  const [topPage, setTopPage] = useState<boolean>(true);
+
+  useEffect( ()=> {
+
+    const handleScroll = function() {
+
+      if (window.scrollY === 0) {
+        setTopPage(true);
+        setSelectPage(SelectPage.Home);
+      }
+
+      if (window.scrollY !== 0) setTopPage(false);
+
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return ()=> window.removeEventListener("scroll", handleScroll);
+
+  }, [])
 
   return (
 
     <div className='app bg-zinc-900 text-slate-50'>
       
-      <Navbar selectPage={selectPage} setSelectPage = {setSelectPage} />
+      <Navbar topPage = {topPage} selectPage={selectPage} setSelectPage = {setSelectPage} />
+      <Home setSelectPage = {setSelectPage} />
 
     </div>
 
