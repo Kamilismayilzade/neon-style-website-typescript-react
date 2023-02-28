331e46b316f84d304d6541bf57538a1c2841b0e8
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-28 12:37:36 +0400

    Home component added

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,21 @@
+import { SelectPage } from '@/shared/types'
+import React from 'react'
+
+type HomeProps = {
+
+    setSelectPage: (value: SelectPage) => void,
+
+}
+
+const Home = ({ setSelectPage }: HomeProps) => {
+
+    
+
+  return (
+
+    <div>Home</div>
+
+  )
+}
+
+export default Home
